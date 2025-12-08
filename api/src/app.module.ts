fd28537662a8fb2ffe2629a7b8232e1122ebef6e
herasb6-svg <herasb6@gmail.com>
2025-12-07 18:38:51 -0600

    limpieza de proyecto - solo funcionalidad de empleados

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TareaModule } from './tareas/tarea.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Tarea } from './tareas/entities/tarea.entity';
-import { Usuario } from './usuarios/entities/usuario.entity';
-import { UsuariosModule } from './usuarios/usuarios.module';
 //import { SensorModule } from './sensor/sensor.module';
 //import { MongooseModule } from '@nestjs/mongoose';
 import { Empleado } from './empleados/entities/empleado.entity';
@@ -15,31 +11,6 @@ import { EmpleadosModule } from './empleados/empleados.module';
 
 @Module({
   imports: [
-  // MongooseModule.forRoot("mongodb://localhost:27017/DSM43"),
-    TypeOrmModule.forRoot({
-        name: "conexion-mariadb",
-        type: "mariadb",
-        host: "localhost",
-        port: 3306,
-        username: "najimi",
-        password: "pass",
-        database: "dsm43",
-        entities: [ Tarea ],
-        synchronize: true,
-        autoLoadEntities: true,
-    }),
-    TypeOrmModule.forRoot({
-        name: "conexion-postgres",
-        type: "postgres",
-        host: "localhost",
-        port: 5432,
-        username: "najimi",
-        password: "pass",
-        database: "empresa",
-        entities: [ Usuario ],
-        synchronize: true,
-        autoLoadEntities: true,
-    }),
     TypeOrmModule.forRoot({
         name: "conexion-postgres",
         type: "postgres",
@@ -52,8 +23,6 @@ import { EmpleadosModule } from './empleados/empleados.module';
         synchronize: true,
         autoLoadEntities: true,
     }),
-    TareaModule,
-    UsuariosModule,
    // SensorModule,
     EmpleadosModule
   ],
